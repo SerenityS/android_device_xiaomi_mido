9bb309df97f7171b740175dec114d599912fe398
HeeRyun, Jin <jins4218@gmail.com>
2017-11-28 23:32:01 +0900

    wifi: Using prebuilt wlan module

--- device.mk
+++ device.mk
@@ -271,3 +271,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/fstman.ini:system/etc/wifi/fstman.ini \
     $(LOCAL_PATH)/wifi/WCNSS_cfg.dat:system/etc/firmware/wlan/prima/WCNSS_cfg.dat \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/wifi/pronto_wlan.ko:/system/lib/modules/pronto/pronto_wlan.ko \
